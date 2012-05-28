2db137e4a2dfdbc33878f0f95cb7d4a25d7e5021
nikhil <nikhil@manan.cs>
2012-05-28 12:49:13 +0530

    adding new repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Testcapistrno::Application.config.secret_token = 'a07bc0c34535bda187a952210c21d3d33a39ea2004b4d07a26d2a73f86f0d06ff6ba7e0cf784bd4dd8118ffc5fcb9f484bc9dec646ba35c03c6e625b3ea6cda1'
